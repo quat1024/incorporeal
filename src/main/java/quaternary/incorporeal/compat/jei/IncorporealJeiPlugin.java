027ce6319f8e4f6f40d12fe4c978ebcda2ddcc3b
Hubry <Hubry@users.noreply.github.com>
2019-03-06 22:24:31 +0100

    Subtype interpreter for nbt subtype items
    Also nonnull annots for the JEI plugin and ignoring a default IDEA build dir

--- src/main/java/quaternary/incorporeal/compat/jei/IncorporealJeiPlugin.java
+++ src/main/java/quaternary/incorporeal/compat/jei/IncorporealJeiPlugin.java
@@ -2,13 +2,21 @@ package quaternary.incorporeal.compat.jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import quaternary.incorporeal.item.cygnus.IncorporeticCygnusItems;
 import quaternary.incorporeal.recipe.skytouch.IncorporeticSkytouchingRecipes;
 import quaternary.incorporeal.recipe.skytouch.RecipeSkytouching;
 
 @JEIPlugin
 public class IncorporealJeiPlugin implements IModPlugin {
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistry reg) {
+		reg.registerSubtypeInterpreter(IncorporeticCygnusItems.WORD_CARD, stack -> IncorporeticCygnusItems.WORD_CARD.readValueName(stack).toString());
+		reg.registerSubtypeInterpreter(IncorporeticCygnusItems.CRYSTAL_CUBE_CARD, stack -> IncorporeticCygnusItems.CRYSTAL_CUBE_CARD.readValueName(stack).toString());
+	}
+
 	@Override
 	public void registerCategories(IRecipeCategoryRegistration reg) {
 		reg.addRecipeCategories(new SkytouchRecipeCategory(reg.getJeiHelpers()));
---
+++ src/main/java/quaternary/incorporeal/compat/jei/package-info.java
@@ -0,0 +1,3 @@
+@javax.annotation.ParametersAreNonnullByDefault
+@mcp.MethodsReturnNonnullByDefault
+package quaternary.incorporeal.compat.jei;
