d6ce343febb0e8360ec43a9c174d3e45bbf1b551
quat <quat1024@users.noreply.github.com>
2019-11-06 03:14:05 -0500

    Lang cleanups

--- src/main/java/quaternary/incorporeal/feature/decorative/block/BlockForgottenShrine.java
+++ src/main/java/quaternary/incorporeal/feature/decorative/block/BlockForgottenShrine.java
@@ -4,14 +4,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import quaternary.incorporeal.feature.decorative.tile.TileSpiritShrineExt;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class BlockForgottenShrine extends Block {
 	public BlockForgottenShrine() {
@@ -48,5 +53,11 @@ public class BlockForgottenShrine extends Block {
 	@Override
 	public TileEntity createTileEntity(World world, IBlockState state) {
 		return new TileSpiritShrineExt();
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(I18n.format("incorporeal.wip"));
 	}
 }
